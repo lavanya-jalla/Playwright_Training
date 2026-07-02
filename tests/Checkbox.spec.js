838561c7712a3c0085d5b2cd8383e73ba52c74ae
Jalla Lavanya <lavanya123jalla@gmail.com>
2026-07-02 10:18:07 +0530

    new files

--- tests/Checkbox.spec.js
+++ tests/Checkbox.spec.js
@@ -10,8 +10,8 @@ test('Checkbox test',async({page})=>{
 test.only('Checkbox test with click',async({page})=>{
     await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
     // await page.locator('#checkbox-example').click();
-    await page.locator('#checkbox-example').nth(1).click();
+   await page.getByRole('checkbox').nth(1).click();
     await page.waitForTimeout(3000);
-    await page.locator('#checkbox-example').nth(2).click();//failing when executed fro mutlple
+    await page.getByRole('checkbox').nth(2).click();//failing when executed fro mutlple
     
 })
